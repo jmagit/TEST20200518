deca8252439d2e6ded24c2e21f484ac1abf7c494
Javier <jmartin@grupoloyal.com>
2020-05-22 13:32:29 +0200

    Version final del curso

--- Tienda.Web/Test/demos.spec.js
+++ Tienda.Web/Test/demos.spec.js
@@ -1,7 +1,9 @@
 ﻿describe('Validaciones', function () {
+
     it('Es un NIF valido', function () {
         expect(isNIF('12345678Z')).toBe(true);
     });
+
     it('Es un NIF incorrecto', function () {
         expect(isNIF('1234A')).toBe(false);
     });
